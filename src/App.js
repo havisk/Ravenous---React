0c4bcaf4eefed2807c8c7ab4c847283f5bc95eb8
Kool Havis <havisk@gmail.com>
2018-04-19 22:17:08 -0400

    static site complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+
 
 class App extends Component {
   render() {
-    return (
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>ravenous</h1>
+        <SearchBar/>
+        <BusinessList/>
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
